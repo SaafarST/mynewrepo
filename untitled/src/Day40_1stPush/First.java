053b9278e9e0f507f2922189571b175328e8cf4b
SaafarST <tohir.s91@gmail.com>
2022-10-04 09:44:03 -0700

    1stPush

---
+++ untitled/src/Day40_1stPush/First.java
@@ -0,0 +1,9 @@
+package Day40_1stPush;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        String string = "Hello \tWorld";
+        System.out.println(string);
+    }
+}
