4466f5a6e3d14a01f983e49740339973ebaa0783
SaafarST <tohir.s91@gmail.com>
2022-10-10 14:17:56 -0700

    Practicewithjavac

---
+++ Week9Project/ReversedOrder.java
@@ -0,0 +1,13 @@
+public class ReversedOrder{
+	public static void main (String[] args)
+	{	
+		 String [] temp  = new String[args.length];
+        for (int i = 0; i < args.length; i++) {
+            temp[args.length-1-i] = args[i];
+        }
+        for (String s : temp) {
+            System.out.print(s+" ");
+
+        }
+	}
+}
