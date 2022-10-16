8dadc2c1141135aaf519a89303cffb6c313a5370
SaafarST <tohir.s91@gmail.com>
2022-10-16 13:31:56 -0700

    Week10_TASK3_working

---
+++ SDET/src/Week10/Task3/getLatestDate.java
@@ -0,0 +1,14 @@
+package Week10.Task3;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class getLatestDate {
+    public LocalDate getDate(ArrayList<String> dates)
+    {
+        LocalDate today = LocalDate.now();
+        for (String date : dates) {
+            
+        }
+    }
+}
