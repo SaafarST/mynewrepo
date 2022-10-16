93f623669a8b770c6b26d59a412d7720a300c1d1
SaafarST <tohir.s91@gmail.com>
2022-10-16 04:17:35 -0700

    Day48

--- SDET/src/Day47/Date/JavaLocalDateTime.java
+++ SDET/src/Day47/Date/JavaLocalDateTime.java
@@ -1,11 +1,18 @@
 package Day47.Date;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 
 public class JavaLocalDateTime {
     public static void main(String[] args) {
         LocalDateTime today = LocalDateTime.now();
         System.out.println(today);
-
+        DateTimeFormatter fullforma = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL);
+        String fullformat = today.format(fullforma);
+        System.out.println(fullformat);
+        DateTimeFormatter ownformat = DateTimeFormatter.ofPattern("MM:dd:yyy|HH:mm:ss");
+        String ownform = today.format(ownformat);
+        System.out.println(ownform);
     }
 }
