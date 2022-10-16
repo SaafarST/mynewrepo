4f2b0befc6158cbbb5dfd43c15e0dfdd754db35a
SaafarST <tohir.s91@gmail.com>
2022-10-16 10:09:37 -0700

    Week10

---
+++ SDET/src/Week10/Task2/Registration.java
@@ -0,0 +1,23 @@
+package Week10.Task2;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Scanner;
+
+public class Registration {
+    public LocalDateTime startTime;
+
+/*    public List<User> register(){
+
+        Scanner scanner = new Scanner(System.in);
+        String scan = " ";
+        while (scan.equals("EXIT")==false)
+        {
+            scan = scanner.next();
+            User user = new User(scan);
+
+        }
+        return user;
+    }*/
+
+}
