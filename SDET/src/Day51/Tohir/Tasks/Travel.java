ab3aab8410be7722ed8b58319a4ecfc1726e3a62
SaafarST <tohir.s91@gmail.com>
2022-10-20 21:06:11 -0700

    Day51

--- SDET/src/Day51/Tohir/Tasks/Travel.java
+++ SDET/src/Day51/Tohir/Tasks/Travel.java
@@ -5,18 +5,35 @@ public class Travel {
     public String Weather;
 
     public static void main(String[] args) {
-        traveltoOcean("HOT");
+        traveltoOcean(Day51.Tohir.Tasks.Weather.HOT);
+       // traveltoOcean("COLD");
     }
 
 
-    public static void traveltoOcean(String weather){
-        if (weather.equals("HOT")){
+    public static void traveltoOcean(Weather weather){
+        /*if (weather == Day51.Tohir.Tasks.Weather.HOT){
             System.out.println("You may travel to Ocean");
         }else {
-            throw new StringIndexOutOfBoundsException();
+            throw new StringIndexOutOfBoundsException("Weather is cold, sit at home");
+        }*/
+        switch (weather){
+            case COLD:
+                throw new RuntimeException("Weaher is cold, stay at home");
+            case HOT:
+                throw new RuntimeException("Best weather to travel to ocean");
+            case WARM:
+                throw new RuntimeException("Not best weather to travel to ocean");
         }
-
     }
 
-    public static void traveltoMountain(){}
+    public static void traveltoMountain(Weather weather) throws NoSuchMethodException {
+        switch (weather){
+            case COLD:
+                throw new NoSuchMethodException("Weaher is cold, stay at home");
+            case HOT:
+                throw new NoSuchMethodException("Not best weather to travel to mountains");
+            case WARM:
+                throw new NoSuchMethodException("Best weather to travel to mountains");
+        }
+    }
 }
