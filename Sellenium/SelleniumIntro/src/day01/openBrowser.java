a0ae25b5125ace880a19181e04f774caa17115c7
SaafarST <tohir.s91@gmail.com>
2022-11-09 13:37:38 -0800

    refresh

---
+++ Sellenium/SelleniumIntro/src/day01/openBrowser.java
@@ -0,0 +1,17 @@
+package day01;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class openBrowser {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+        WebDriver webDriver = new ChromeDriver();
+
+        webDriver.get("https://www.google.com");
+        String title = webDriver.getTitle();
+        System.out.println("Title is "+title);
+
+        webDriver.quit();
+    }
+}
