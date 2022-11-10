59fdb070379fc281674f933981da22abf28d3ade
SaafarST <tohir.s91@gmail.com>
2022-11-10 08:19:18 -0800

    Day1

--- Sellenium/SelleniumIntro/src/day01/openOpera.java
+++ Sellenium/SelleniumIntro/src/day01/openOpera.java
@@ -1,7 +1,13 @@
 package day01;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class openOpera {
     public static void main(String[] args) {
-        
+        System.out.println("hello Aydin!");
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("https://yandex.ru");
+        System.out.println("URL is "+webDriver.getTitle());
     }
 }
