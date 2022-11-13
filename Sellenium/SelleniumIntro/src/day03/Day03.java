bc02e0849b180a369eee6eff16ac57ac74b81a1d
SaafarST <tohir.s91@gmail.com>
2022-11-13 13:30:22 -0800

    day03

--- Sellenium/SelleniumIntro/src/day03/Day03.java
+++ Sellenium/SelleniumIntro/src/day03/Day03.java
@@ -1,4 +1,16 @@
 package day03;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Day03 {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demo.guru99.com/test/newtours/");
+        driver.findElement(By.linkText("REGISTER")).click();
+
+
+    }
 }
--- Sellenium/SelleniumIntro/src/day03/WebElements.java
+++ Sellenium/SelleniumIntro/src/day03/WebElements.java
@@ -1,4 +1,21 @@
 package day03;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class WebElements {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://ok.ru");
+        Thread.sleep(2000);
+        driver.findElement(By.name("st.email")).sendKeys("test@gmail.com");
+        Thread.sleep(2000);
+        driver.findElement(By.id("field_password")).sendKeys("12345");
+        Thread.sleep(2000);
+        driver.findElement(By.partialLinkText("Log in to OK")).click();
+        Thread.sleep(2000);
+        driver.close();
+    }
 }
