dc338a1b20f4c76eedebb46579d3ccf3e335d251
SaafarST <tohir.s91@gmail.com>
2022-11-13 00:03:45 -0800

    day02

---
+++ Sellenium/SelleniumIntro/src/day02/openFacebook.java
@@ -0,0 +1,15 @@
+package day02;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class openFacebook {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("https://vk.ru");
+        System.out.println("Title is "+webDriver.getTitle());
+        webDriver.quit();
+
+    }
+}
