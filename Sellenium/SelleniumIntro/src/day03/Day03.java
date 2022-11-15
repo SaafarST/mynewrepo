09b26a69d8dda6e277d021d1012bf0bcdfab1e46
SaafarST <tohir.s91@gmail.com>
2022-11-14 17:10:09 -0800

    day03

--- Sellenium/SelleniumIntro/src/day03/Day03.java
+++ Sellenium/SelleniumIntro/src/day03/Day03.java
@@ -5,11 +5,25 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Day03 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://demo.guru99.com/test/newtours/");
+        Thread.sleep(1000);
         driver.findElement(By.linkText("REGISTER")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.name("firstName")).sendKeys("John");
+        driver.findElement(By.name("lastName")).sendKeys("Sick");
+        driver.findElement(By.name("phone")).sendKeys("111222");
+        driver.findElement(By.name("email")).sendKeys("JWick");
+        driver.findElement(By.name("password")).sendKeys("jwick1122");
+        driver.findElement((By.name("confirmPassword"))).sendKeys("jwick1122");
+        driver.findElement(By.name("submit")).click();
+        Thread.sleep(2500);
+        driver.close();
+
+
+        //driver.close();
 
 
     }
