c7bd9f85601410b17145edd9341cf5abf9412f73
SaafarST <tohir.s91@gmail.com>
2022-11-14 17:13:42 -0800

    day03

--- Sellenium/SelleniumIntro/src/day03/Day03.java
+++ Sellenium/SelleniumIntro/src/day03/Day03.java
@@ -13,11 +13,17 @@ public class Day03 {
         driver.findElement(By.linkText("REGISTER")).click();
         Thread.sleep(1000);
         driver.findElement(By.name("firstName")).sendKeys("John");
+        Thread.sleep(100);
         driver.findElement(By.name("lastName")).sendKeys("Sick");
+        Thread.sleep(100);
         driver.findElement(By.name("phone")).sendKeys("111222");
+        Thread.sleep(100);
         driver.findElement(By.name("email")).sendKeys("JWick");
+        Thread.sleep(100);
         driver.findElement(By.name("password")).sendKeys("jwick1122");
+        Thread.sleep(100);
         driver.findElement((By.name("confirmPassword"))).sendKeys("jwick1122");
+        Thread.sleep(100);
         driver.findElement(By.name("submit")).click();
         Thread.sleep(2500);
         driver.close();
