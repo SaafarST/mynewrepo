5c1c1cbc5dd3ef23207da27cba30ece20436791e
SaafarST <tohir.s91@gmail.com>
2022-11-20 22:11:08 -0800

    class04

--- Sellenium/SelleniumIntro/src/Class04/XPathDemo.java
+++ Sellenium/SelleniumIntro/src/Class04/XPathDemo.java
@@ -1,7 +1,14 @@
 package Class04;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class XPathDemo {
+    private static String URL="https://exelentersdet.com";
     public static void main(String[] args) {
-        
+        System.setProperty("webdriver/chrome/driver","drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get(URL);
+
     }
 }
