f495ee0477864fda604386101cda125f9f345229
SaafarST <tohir.s91@gmail.com>
2022-11-21 08:51:25 -0800

    xPath

--- Sellenium/SelleniumIntro/src/Class04/XPathDemo.java
+++ Sellenium/SelleniumIntro/src/Class04/XPathDemo.java
@@ -1,14 +1,27 @@
 package Class04;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class XPathDemo {
     private static String URL="https://exelentersdet.com";
-    public static void main(String[] args) {
-        System.setProperty("webdriver/chrome/driver","drivers/chromedriver.exe");
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get(URL);
-
+        //driver.findElement(By.name("txtUsername")).sendKeys("Hi");
+        String absolutexPath = "/html/body/div/div/div/div/div[2]/form/div[2]/input";
+        String relativexPath = "//input[@name='txtUsername']";
+        String absolutexPathChrome = "/html/body/div[1]/div/div[2]/div[2]/div[2]/form/div[2]/input";
+        String realtivexPathChrome = "//*[@id=\"divUsername\"]";
+        String xPathpassword = "//*[@id=\"txtPassword\"]";
+        String xPathLogin = "//input[@value='LOGIN']";
+        driver.findElement(By.xpath(absolutexPath)).sendKeys("Admin");
+        driver.findElement(By.xpath(xPathpassword)).sendKeys("Exelent2022Sdet!");
+        driver.findElement(By.xpath(xPathLogin)).click();
+        
+        Thread.sleep(3000);
+        //driver.close();
     }
 }
