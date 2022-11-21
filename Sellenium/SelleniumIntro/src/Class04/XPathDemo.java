7861c42431a26d705e5638241c60c13b80cf8372
SaafarST <tohir.s91@gmail.com>
2022-11-21 07:58:48 -0800

    xPath

--- Sellenium/SelleniumIntro/src/Class04/XPathDemo.java
+++ Sellenium/SelleniumIntro/src/Class04/XPathDemo.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class XPathDemo {
     private static String URL="https://exelentersdet.com";
     public static void main(String[] args) throws InterruptedException {
+        
         System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get(URL);
