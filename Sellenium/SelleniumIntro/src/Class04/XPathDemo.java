2fe5de6961c21fd42c42c1788de4001544e4585b
SaafarST <tohir.s91@gmail.com>
2022-11-22 22:17:54 -0800

    xPath

--- Sellenium/SelleniumIntro/src/Class04/XPathDemo.java
+++ Sellenium/SelleniumIntro/src/Class04/XPathDemo.java
@@ -12,16 +12,19 @@ public class XPathDemo {
         WebDriver driver = new ChromeDriver();
         driver.get(URL);
         String absolutexPath = "/html/body/div/div/div/div/div[2]/form/div[2]/input";
+
         String relativexPath = "//input[@name='txtUsername']";
+
         String absolutexPathChrome = "/html/body/div[1]/div/div[2]/div[2]/div[2]/form/div[2]/input";
         String realtivexPathChrome = "//*[@id=\"divUsername\"]";
+
         String xPathpassword = "//*[@id=\"txtPassword\"]";
         String xPathLogin = "//input[@value='LOGIN']";
 
         String xPathCOntainsByText = "//a[contains(text(),'Welcome')]";
         String xPathContainsByText01 = "//a[contains(text(),'Logout')]";
         String xPathContainsByAttribute = "//a[contains(@class,'panel')]";
-        
+
         driver.findElement(By.xpath(absolutexPath)).sendKeys("Admin");
         driver.findElement(By.xpath(xPathpassword)).sendKeys("Exelent2022Sdet!");
         driver.findElement(By.xpath(xPathLogin)).click();
