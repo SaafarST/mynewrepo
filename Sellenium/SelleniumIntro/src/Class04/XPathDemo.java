adec3bf17a10e413fa4989313266bc616fc7b1e9
SaafarST <tohir.s91@gmail.com>
2022-11-22 22:06:45 -0800

    xPath

--- Sellenium/SelleniumIntro/src/Class04/XPathDemo.java
+++ Sellenium/SelleniumIntro/src/Class04/XPathDemo.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class XPathDemo {
     private static String URL="https://exelentersdet.com";
     public static void main(String[] args) throws InterruptedException {
-        
+
         System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get(URL);
@@ -17,10 +17,13 @@ public class XPathDemo {
         String realtivexPathChrome = "//*[@id=\"divUsername\"]";
         String xPathpassword = "//*[@id=\"txtPassword\"]";
         String xPathLogin = "//input[@value='LOGIN']";
+        String xPathContainsByAttribute = "//a[contains(@class,'panel')]";
+        String xPathCOntainsByText = "//a[contains(text(),'Welcome')]";
         driver.findElement(By.xpath(absolutexPath)).sendKeys("Admin");
         driver.findElement(By.xpath(xPathpassword)).sendKeys("Exelent2022Sdet!");
         driver.findElement(By.xpath(xPathLogin)).click();
+        driver.findElement(By.xpath(xPathCOntainsByText)).click();
         Thread.sleep(3000);
-        //driver.close();
+        driver.close();
     }
 }
