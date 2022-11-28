37d4d78d89b744e8610d2f9c40749290c558ce95
SaafarST <tohir.s91@gmail.com>
2022-11-28 11:58:48 -0800

    newCommands

---
+++ Sellenium/SelleniumIntro/src/Class05/Class05/recap.java
@@ -0,0 +1,9 @@
+package Class05.Class05;
+
+public class recap {
+    public static void main(String[] args) {
+        String webElement = "<a href=\"#\" id=\"welcome\" class=\"panelTrigger\">Welcome Admin</a>";
+        //xPath-->//input[@id='welcome'] //input[text()='Welcome Admin'] //input[contains(text(),'come'] //input[@class='panelTrigger']
+        //CSS-->input#welcome input.panelTrigger input[id='welcome'] input[id*=com]
+    }
+}
