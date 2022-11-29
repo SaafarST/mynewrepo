e2bbc5f582000cfc69255675012275f25516a2ad
SaafarST <tohir.s91@gmail.com>
2022-11-29 07:53:11 -0800

    radio

--- Sellenium/SelleniumIntro/src/Class06/radioDemo_01.java
+++ Sellenium/SelleniumIntro/src/Class06/radioDemo_01.java
@@ -1,9 +1,14 @@
 package Class06;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class radioDemo_01 {
     private static String url = "https://selenium08.blogspot.com/2019/07/check-box-and-radio-buttons.html";
 
     public static void main(String[] args) {
-
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get(url);
     }
 }
