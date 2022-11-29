74d9632c0d54d68fb413e47063a3a0cfcd04bc3c
SaafarST <tohir.s91@gmail.com>
2022-11-29 08:15:02 -0800

    radio

--- Sellenium/SelleniumIntro/src/Class06/radioDemo_02.java
+++ Sellenium/SelleniumIntro/src/Class06/radioDemo_02.java
@@ -2,6 +2,7 @@ package Class06;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class radioDemo_02 {
@@ -12,7 +13,10 @@ public class radioDemo_02 {
         WebDriver driver = new ChromeDriver();
         driver.get(url);
 
-        
+        String cssThree = "input[value='3']";//or input#checkbox-3
+        WebElement checkBoxThree = driver.findElement(By.cssSelector(cssThree));
+         boolean checkBoxThreeEnabled = checkBoxThree.isEnabled();
+        System.out.println(checkBoxThreeEnabled);
         Thread.sleep(2000);
         driver.close();
     }
