805efbf9f987998440c6a30267d88ef9276f28c7
SaafarST <tohir.s91@gmail.com>
2022-11-29 08:01:43 -0800

    radio

--- Sellenium/SelleniumIntro/src/Class06/radioDemo_01.java
+++ Sellenium/SelleniumIntro/src/Class06/radioDemo_01.java
@@ -1,14 +1,26 @@
 package Class06;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class radioDemo_01 {
     private static String url = "https://selenium08.blogspot.com/2019/07/check-box-and-radio-buttons.html";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get(url);
+
+        String xPathMozilla = "//input[@name='browser'][2]";//css = input[value='Mozilla']
+        driver.findElement(By.xpath(xPathMozilla)).click();
+
+         boolean displayed = driver.findElement(By.xpath(xPathMozilla)).isDisplayed();
+         boolean enabled = driver.findElement(By.xpath(xPathMozilla)).isEnabled();
+         boolean selected = driver.findElement(By.xpath(xPathMozilla)).isSelected();
+
+        System.out.println(displayed+" "+enabled+" "+selected);
+        Thread.sleep(2000);
+        driver.close();
     }
 }
