2088d5511bdcf218a85775dbb5c1cd03e2da2dc3
SaafarST <tohir.s91@gmail.com>
2022-11-29 07:52:09 -0800

    findByAttribute

---
+++ Sellenium/SelleniumIntro/src/Class06/radioDemo_01.java
@@ -0,0 +1,9 @@
+package Class06;
+
+public class radioDemo_01 {
+    private static String url = "https://selenium08.blogspot.com/2019/07/check-box-and-radio-buttons.html";
+
+    public static void main(String[] args) {
+
+    }
+}
