09238f12106e935fc511498ff2d01d3aeed886f5
SaafarST <tohir.s91@gmail.com>
2022-11-29 20:40:52 -0800

    radioPart2

--- Sellenium/SelleniumIntro/src/Class06/radioDemo_02.java
+++ Sellenium/SelleniumIntro/src/Class06/radioDemo_02.java
@@ -13,12 +13,22 @@ public class radioDemo_02 {
         WebDriver driver = new ChromeDriver();
         driver.get(url);
 
+
+        String cssOne = "input#checkbox-1";
+        WebElement checkBoxOne = driver.findElement(By.cssSelector(cssOne));
+         boolean checkBoxOneSelected = checkBoxOne.isSelected();
+        System.out.println("check box one is selected = "+ checkBoxOneSelected);
+
+        String cssTwo = "input#checkbox-2";// xPath (//input[@class='fs-checkbox-element'])[2]
+         WebElement checkBocTwo = driver.findElement(By.cssSelector(cssTwo));
+         boolean checkBocTwoEnabled = checkBocTwo.isEnabled();
+        System.out.println("checkBocTwo is Enabled = " + checkBocTwoEnabled);
+
         String cssThree = "input[value='3']";//or input#checkbox-3
         WebElement checkBoxThree = driver.findElement(By.cssSelector(cssThree));
-         boolean checkBoxThreeEnabled = checkBoxThree.isEnabled();
+        boolean checkBoxThreeEnabled = checkBoxThree.isEnabled();
         System.out.println(checkBoxThreeEnabled);
 
-        
         Thread.sleep(2000);
         driver.close();
     }
