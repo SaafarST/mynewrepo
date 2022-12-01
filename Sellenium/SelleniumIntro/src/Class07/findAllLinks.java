813b1cc7ddf8ad200df23bc8378acc396d9b6d5f
SaafarST <tohir.s91@gmail.com>
2022-12-01 08:16:11 -0800

    findAllLinks

--- Sellenium/SelleniumIntro/src/Class07/findAllLinks.java
+++ Sellenium/SelleniumIntro/src/Class07/findAllLinks.java
@@ -19,10 +19,14 @@ public class findAllLinks {
         List<WebElement> allLinks = driver.findElements(By.tagName("a"));
         System.out.println("allLinks = " + allLinks.size());
 
+        int count = 0;
         for(WebElement link:allLinks){
+            if (!link.getText().isEmpty()&&!link.getText().isBlank()){
             System.out.println(link.getText());
-        }
+            count++;}
 
+        }
+        System.out.println("Number of links with text: "+count);
         Thread.sleep(2000);
         driver.close();
     }
