c52d0c09f3589db6d62b3bc48f955317a6936473
SaafarST <tohir.s91@gmail.com>
2022-12-01 01:55:00 -0800

    radioCheckbox

---
+++ Sellenium/SelleniumIntro/src/Class06/TC_RadioCheckbox.java
@@ -0,0 +1,32 @@
+package Class06;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+    public class TC_RadioCheckbox {
+    private static String url = "https://selenium08.blogspot.com/2019/07/check-box-and-radio-buttons.html";
+
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get(url);
+        String cssYellow = "input[value='yellow']";
+        String cssOrange = "input[value='orange']";
+        driver.findElement(By.cssSelector(cssOrange)).;
+        driver.findElement(By.cssSelector(cssYellow)).submit();
+
+        List<WebElement> elements = driver.findElements(By.name("color"));
+        for(WebElement element:elements){
+            if (element.isSelected()){
+                System.out.println(element.getAttribute("value"));
+            }
+        }
+
+        Thread.sleep(3000);
+        driver.close();
+    }
+}
