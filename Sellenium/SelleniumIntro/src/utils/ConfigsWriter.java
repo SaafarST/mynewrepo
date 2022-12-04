72a087e490e95b982c4c64ab3fe62e7291270ffc
SaafarST <tohir.s91@gmail.com>
2022-12-04 04:11:26 -0800

    config

--- Sellenium/SelleniumIntro/src/utils/ConfigsWriter.java
+++ Sellenium/SelleniumIntro/src/utils/ConfigsWriter.java
@@ -5,15 +5,14 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
-public class ConfigsReader {
+public class ConfigsWriter {
     public static void main(String[] args) throws IOException {
         String filePath = System.getProperty("user.dir")+"/configs/configuration.properties";
         FileInputStream fis = new FileInputStream(filePath);
         Properties properties = new Properties();
         properties.load(fis);
 
-        final String browser = properties.getProperty("browser");
-        System.out.println("browser = " + browser);
+        
 
     }
 }
