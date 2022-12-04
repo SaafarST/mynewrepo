579b21c70e7917cd81c56d16befec91a581de4ab
SaafarST <tohir.s91@gmail.com>
2022-12-03 23:04:27 -0800

    class09

---
+++ Sellenium/SelleniumIntro/src/Class08/Cars.java
@@ -0,0 +1,13 @@
+package Class08;
+
+public abstract class Cars {
+
+    String fuelType;
+
+    public Cars(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    abstract void fuelType();
+
+}
