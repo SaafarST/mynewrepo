1b3d38a9cf00b3e8faff2be28d6bcc374aade49d
SaafarST <tohir.s91@gmail.com>
2022-12-03 22:49:10 -0800

    class09

--- Sellenium/SelleniumIntro/src/Class08/_01_IteratorIntro.java
+++ Sellenium/SelleniumIntro/src/Class08/_01_IteratorIntro.java
@@ -1,6 +1,7 @@
 package Class08;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class _01_IteratorIntro {
@@ -13,7 +14,26 @@ public class _01_IteratorIntro {
         LdoubleList.add(2.98);
         LdoubleList.add(3.44);
         System.out.println(LdoubleList);
-        System.out.println(LdoubleList.get(2));
+        System.out.println(LdoubleList.get(5));
+
+        System.out.println("---For Loop---");
+        for (int i = 0; i < LdoubleList.size(); i++) {
+            System.out.println(LdoubleList.get(i));
+            System.out.println("Using doubleValue method: "+LdoubleList.get(i).doubleValue());
+            System.out.println("Using intValue method: "+LdoubleList.get(i).doubleValue());
+        }
+
+        System.out.println("---Enhanced Loop---");
+        for(double dlist:LdoubleList){
+            System.out.println(dlist);
+        }
+
+        System.out.println("---Iterator Loop---");
+        Iterator<Double> doubleIterator = LdoubleList.iterator();
+        while (doubleIterator.hasNext()){
+            System.out.println(doubleIterator.next());
+        }
+
 
     }
 }
