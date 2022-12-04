31647464af0a9d37c9e101fdad63caf8a693af31
SaafarST <tohir.s91@gmail.com>
2022-12-04 03:34:54 -0800

    config

--- Sellenium/SelleniumIntro/src/Class09/_01_ConnectWithPropertierFile.java
+++ Sellenium/SelleniumIntro/src/Class09/_01_ConnectWithPropertierFile.java
@@ -1,12 +1,15 @@
 package Class09;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class _01_ConnectWithPropertierFile {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         //String filePath = "C:\\Users\\Nju\\Desktop\\SD\\OwnRepo\\mynewrepo\\Sellenium\\SelleniumIntro\\configs\\configuration.properties";
         String filePath = System.getProperty("user.dir")+"/configs/configuration.properties";
         System.out.println("filePath = " + filePath);
-
 
         String fullPath_forCSS = "C:\\Users\\Nju\\Desktop\\SD\\OwnRepo\\mynewrepo\\Sellenium\\SelleniumIntro\\src\\Class04\\cssDemo.java";
         System.out.println("fullPath_forCSS = " + fullPath_forCSS);
@@ -14,7 +17,12 @@ public class _01_ConnectWithPropertierFile {
         String shortPath_forCSS =System.getProperty("user.dir")+"/src/Class04/cssDemo.java";
         System.out.println("shortPath_forCSS = " + shortPath_forCSS);
 
-        
+        FileInputStream fileInputStream = new FileInputStream(filePath);
+
+        Properties properties = new Properties();
+        properties.load(fileInputStream);
+
+
 
 
     }
