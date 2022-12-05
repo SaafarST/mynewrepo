5f2484939dc57fa11778c90b96770b78fa36ae45
SaafarST <tohir.s91@gmail.com>
2022-12-05 11:08:55 -0800

    config

--- Sellenium/SelleniumIntro/src/Class09/_02_LoginToExelenter.java
+++ Sellenium/SelleniumIntro/src/Class09/_02_LoginToExelenter.java
@@ -1,8 +1,15 @@
 package Class09;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 public class _02_LoginToExelenter {
     public static void main(String[] args) {
-        
+
+        String filePath = System.getProperty("user.dir")+"/configs/configuration.properties";
+        FileInputStream fis = new FileInputStream(filePath);
+        Properties properties = new Properties();
+        properties.load(fis);
 
     }
 }
