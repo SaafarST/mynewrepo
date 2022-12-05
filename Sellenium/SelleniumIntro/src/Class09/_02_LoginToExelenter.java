53864bb4dcb959082a4016cfad52b464797894d0
SaafarST <tohir.s91@gmail.com>
2022-12-05 11:16:37 -0800

    config

--- Sellenium/SelleniumIntro/src/Class09/_02_LoginToExelenter.java
+++ Sellenium/SelleniumIntro/src/Class09/_02_LoginToExelenter.java
@@ -24,7 +24,7 @@ public class _02_LoginToExelenter {
                 driver = new ChromeDriver();
                 break;
             case "firefox":
-                System.setProperty("webdriver.chrome.driver","drivers/geckodriver");
+                System.setProperty("webdriver.gecko.driver","drivers/geckodriver");
                 driver = new FirefoxDriver();
                 break;
         }
