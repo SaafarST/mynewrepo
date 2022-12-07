bbc24ce24774282047e1c730d2c45af8ce655d39
SaafarST <tohir.s91@gmail.com>
2022-12-06 17:03:33 -0800

    class10

--- Sellenium/SelleniumIntro/src/utils/ConfigsReaderold.java
+++ Sellenium/SelleniumIntro/src/utils/ConfigsReaderold.java
@@ -1,11 +1,10 @@
 package utils;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
-public class ConfigsReader {
+public class ConfigsReaderold {
     public static void main(String[] args) throws IOException {
         String filePath = System.getProperty("user.dir")+"/configs/configuration.properties";
         FileInputStream fis = new FileInputStream(filePath);
@@ -15,11 +14,5 @@ public class ConfigsReader {
         final String browser = properties.getProperty("browser");
         System.out.println("browser = " + browser);
 
-    }
-    public static void loadProperties(String filePath) throws IOException {
-
-        FileInputStream fis = new FileInputStream(filePath);
-        Properties properties = new Properties();
-        properties.load(fis);
     }
 }
