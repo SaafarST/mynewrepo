d8d59953d38f7acbda0ea2dae89d0581e668662a
SaafarST <tohir.s91@gmail.com>
2022-12-07 20:10:27 -0800

    systemConfig

--- Sellenium/SelleniumIntro/src/utils/BaseClass.java
+++ Sellenium/SelleniumIntro/src/utils/BaseClass.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BaseClass {
 
+    public static WebDriver driver;
+
     public static void setUp() {
 
         ConfigsReader.loadProperties(Constants.CONFIGURATION_FILEPATH);
@@ -15,14 +17,15 @@ public class BaseClass {
         switch (ConfigsReader.getProperties("browser").toLowerCase()) {
             case "chrome" -> {
                 System.setProperty("webdriver.chrome.driver", Constants.CHROME_DRIVER_PATH);
-                WebDriver driver = new ChromeDriver();
+                driver = new ChromeDriver();
             }
             case "firefox" -> {
                 System.setProperty("webdriver.msedgedriver.driver", "drivers/gecko.exe");
-                WebDriver driver = new FirefoxDriver();
+                driver = new FirefoxDriver();
             }
             default -> throw new RuntimeException("Browser is not imported");
         }
+        driver.get(ConfigsReader.getProperties("url"));
 
     }
 }
