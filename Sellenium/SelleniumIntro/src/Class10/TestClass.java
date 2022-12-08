282897db07bf8480afeeb80e998646a90c3d5714
SaafarST <tohir.s91@gmail.com>
2022-12-07 20:03:57 -0800

    testClass

--- Sellenium/SelleniumIntro/src/Class10/TestClass.java
+++ Sellenium/SelleniumIntro/src/Class10/TestClass.java
@@ -3,7 +3,11 @@ package Class10;
 import utils.BaseClass;
 
 public class TestClass extends BaseClass {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws InterruptedException {
+        setUp();
+
+        System.out.println(driver.getTitle());
+
+        tearDown();
     }
 }
