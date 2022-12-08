ae6a7bd9a41fa78f4c689f8d5a34844b6b2bcd29
SaafarST <tohir.s91@gmail.com>
2022-12-08 08:36:38 -0800

    testClass

--- Sellenium/SelleniumIntro/src/Class10/_01_ExcelIntro.java
+++ Sellenium/SelleniumIntro/src/Class10/_01_ExcelIntro.java
@@ -1,7 +1,20 @@
 package Class10;
 
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class _01_ExcelIntro {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        String filepath = "C:/Users/Nju/Desktop/SD/Excel/ztestfile.xlsx";
+        FileInputStream fis = new FileInputStream(filepath);
+
+        Workbook workbook = new XSSFWorkbook();
+
+
+
 
     }
 }
