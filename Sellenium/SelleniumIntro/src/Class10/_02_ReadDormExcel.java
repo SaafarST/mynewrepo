de21c4a455f88d59c08705a141c6ab87a3699efe
SaafarST <tohir.s91@gmail.com>
2022-12-09 20:54:21 -0800

    ApachePOI_creatingNeqExcel

---
+++ Sellenium/SelleniumIntro/src/Class10/_02_ReadDormExcel.java
@@ -0,0 +1,26 @@
+package Class10;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class _02_ReadDormExcel {
+
+
+    public static void main(String[] args) throws IOException {
+        String filepath = System.getProperty("user.dir")+"/testData/EmployeeList.xlsx";
+        FileInputStream fis = new FileInputStream(filepath);
+
+        Workbook workbook = new XSSFWorkbook(fis);
+        Sheet sheet1 = workbook.getSheet("Sheet1");
+        final Row headerRow = sheet1.getRow(0);
+        
+
+
+    }
+}
