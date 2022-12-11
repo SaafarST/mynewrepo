ab26dbe0ed043c84238709f3c3ac674abcb34eff
SaafarST <tohir.s91@gmail.com>
2022-12-11 00:10:06 -0800

    ApachePOI_WriteToExcel

--- Sellenium/SelleniumIntro/src/Class10/_03_WriteToExcel.java
+++ Sellenium/SelleniumIntro/src/Class10/_03_WriteToExcel.java
@@ -4,6 +4,7 @@ import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class _03_WriteToExcel {
@@ -12,7 +13,14 @@ public class _03_WriteToExcel {
         FileInputStream fis = new FileInputStream(filepath);
 
         Workbook workbook = new XSSFWorkbook(fis);
-        
+        Sheet sheet1 = workbook.getSheet("Sheet1");
+        sheet1.getRow(0).createCell(6).setCellValue("Seniority Level");
+        sheet1.getRow(1).createCell(6).setCellValue("Mid-Level");
+        sheet1.getRow(2).createCell(6).setCellValue("Entry-Level");
+        sheet1.getRow(3).createCell(6).setCellValue("Senior-Level");
+
+        FileOutputStream fos = new FileOutputStream(filepath);
+        workbook.write(fos);
 
 
     }
