4e4e8be36f5f2ebcbc4e1d920a2b83c362233484
SaafarST <tohir.s91@gmail.com>
2022-12-11 00:22:16 -0800

    ApachePOI_WriteToExcel_xlsToMap

---
+++ Sellenium/SelleniumIntro/src/Class10/_04_ExcelToMap.java
@@ -0,0 +1,21 @@
+package Class10;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class _04_ExcelToMap {
+    public static void main(String[] args) throws IOException {
+        String filepath = System.getProperty("user.dir")+"/testData/EmployeeList.xlsx";
+        FileInputStream fis = new FileInputStream(filepath);
+
+        Workbook workbook = new XSSFWorkbook(fis);
+        Sheet sheet1 = workbook.getSheet("Sheet1");
+        
+
+    }
+}
