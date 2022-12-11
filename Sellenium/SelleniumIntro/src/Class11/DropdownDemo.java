4f639e28789863ee5d9e28f729ba3aa055d84daa
SaafarST <tohir.s91@gmail.com>
2022-12-11 02:41:47 -0800

    class11_Dropdown

--- Sellenium/SelleniumIntro/src/Class11/DropdownDemo.java
+++ Sellenium/SelleniumIntro/src/Class11/DropdownDemo.java
@@ -1,16 +1,23 @@
 package Class11;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
 import static utils.BaseClass.*;
 
 
 public class DropdownDemo {
     public static void main(String[] args) {
         setUp();
+        List<WebElement> countries = driver.findElements(By.cssSelector("select[name=country] option"));
+        for (WebElement country:countries)
+        {
+            System.out.println("country = " + country);
+        }
 
-
-
-
-
+        tearDown();
 
     }
 }
