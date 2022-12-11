af0c14b1310522859e91c30a016e55904cf92ac8
SaafarST <tohir.s91@gmail.com>
2022-12-10 23:36:23 -0800

    ApachePOI_WriteToExcel_xlsToMap

--- Sellenium/SelleniumIntro/src/Class10/_04_ExcelToMap.java
+++ Sellenium/SelleniumIntro/src/Class10/_04_ExcelToMap.java
@@ -7,6 +7,9 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.management.ManagementPermission;
+import java.util.HashMap;
+import java.util.Map;
 
 public class _04_ExcelToMap {
     public static void main(String[] args) throws IOException {
@@ -16,10 +19,16 @@ public class _04_ExcelToMap {
         Workbook workbook = new XSSFWorkbook(fis);
         Sheet sheet1 = workbook.getSheet("Sheet1");
 
-        int rows = sheet1.getPhysicalNumberOfRows();
-        System.out.println("rows = " + rows);
-        int columns = sheet1.getRow(0).getLastCellNum();
-        System.out.println("columns = " + columns);
+        int rows = sheet1.getPhysicalNumberOfRows();//<---Rows number
+        int columns = sheet1.getRow(0).getLastCellNum();//<---Columns number
+
+        Map<String,String> map = new HashMap<>();
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < columns; c++) {
+                System.out.print(sheet1.getRow(r).getCell(c)+" ");
+            }
+            System.out.println("");
+        }
 
 
 
