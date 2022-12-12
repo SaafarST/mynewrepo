2653b430e35ed83ea1de42a6b7d232e2a41beb54
SaafarST <tohir.s91@gmail.com>
2022-12-11 21:12:52 -0800

    assinment_01

--- Sellenium/SelleniumIntro/src/Assignments/_01_CheckBoxAndRadioButtons.java
+++ Sellenium/SelleniumIntro/src/Assignments/_01_CheckBoxAndRadioButtons.java
@@ -1,9 +1,23 @@
 package Assignments;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 import static utils.BaseClass.*;
 
 public class _01_CheckBoxAndRadioButtons {
     public static void main(String[] args) {
         setUp();
+        //WebElement checkbox = driver.findElement(By.cssSelector("#content > ul > li:nth-child(6) > a"));
+        WebElement checkboxes = driver.findElement(By.linkText("Checkboxes"));
+        checkboxes.click();
+
+        WebElement checkbox1 = checkboxes.findElement(By.linkText(" checkbox 1"));
+        WebElement checkbox2 = checkboxes.findElement(By.linkText(" checkbox 2"));
+
+        final boolean displayed = checkbox1.isDisplayed();
+
+        tearDown();
+
 
     }
 }
