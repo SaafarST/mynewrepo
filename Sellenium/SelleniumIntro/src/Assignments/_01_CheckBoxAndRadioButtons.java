c1d574ec1dfda297c4235c3a35bbad123cf00f98
SaafarST <tohir.s91@gmail.com>
2022-12-12 20:59:22 -0800

    assinment_01

--- Sellenium/SelleniumIntro/src/Assignments/_01_CheckBoxAndRadioButtons.java
+++ Sellenium/SelleniumIntro/src/Assignments/_01_CheckBoxAndRadioButtons.java
@@ -2,6 +2,9 @@ package Assignments;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static utils.BaseClass.*;
 
 public class _01_CheckBoxAndRadioButtons {
@@ -11,10 +14,16 @@ public class _01_CheckBoxAndRadioButtons {
         WebElement checkboxes = driver.findElement(By.linkText("Checkboxes"));
         checkboxes.click();
 
-        WebElement checkbox1 = checkboxes.findElement(By.xpath("//*[@id=\"checkboxes\"]/text()[2]"));
+        List<WebElement> Checkboxes = driver.findElements(By.xpath("//*[@type='checkbox']"));
 
-        System.out.println("checkbox1.getText() = " + checkbox1.getText());
-        
+        for (WebElement checkbox : Checkboxes) {
+            System.out.println("Checkbox is displayed= "+checkbox.isDisplayed()+", is selected= "+checkbox.isSelected()+", is enabled= "+checkbox.isEnabled());
+
+        }
+
+
+ /*       System.out.println("checkbox1.getText() = " + checkbox1.getText());
+
         WebElement checkbox2 = checkboxes.findElement(By.xpath("//*[@id=\"checkboxes\"]/text()[4]"));
 
         final boolean displayed_cb1 = checkbox1.isDisplayed();
@@ -22,8 +31,8 @@ public class _01_CheckBoxAndRadioButtons {
         final boolean enabled_cb1 = checkbox1.isEnabled();
 
         System.out.println("Checkbox 1 is displayed= "+displayed_cb1+", is selected= "+selected_cb1+", is enabled= "+enabled_cb1);
-
-        //tearDown();
+*/
+        tearDown();
 
 
     }
