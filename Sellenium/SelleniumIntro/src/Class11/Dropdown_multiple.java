7119398edc4fd0c10e1528c7bb49198636c85e19
SaafarST <tohir.s91@gmail.com>
2022-12-13 12:45:46 -0800

    dropdown_multiple

--- Sellenium/SelleniumIntro/src/Class11/Dropdown_multiple.java
+++ Sellenium/SelleniumIntro/src/Class11/Dropdown_multiple.java
@@ -1,9 +1,25 @@
 package Class11;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
 import static utils.BaseClass.*;
 
 public class Dropdown_multiple {
     public static void main(String[] args) {
-        
+
+        setUp();
+
+
+        WebElement months = driver.findElement(By.cssSelector("/html/body/div[1]/div[2]/div[1]/div/div/main/div/div[1]/div/article/div/div/div[3]/div[1]/div/div[3]/select"));
+        Select select = new Select(months);
+
+        List<WebElement> listOfMonths = select.getOptions();
+        int totalMonths = listOfMonths.size();
+        System.out.println("total of Months = " + totalMonths);
+
 
 
 
