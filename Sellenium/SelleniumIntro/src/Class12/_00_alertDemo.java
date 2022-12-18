872b4570049bf8717cb66504c08486c329ce9f02
TohirJu <53301540+SaafarST@users.noreply.github.com>
2022-12-18 17:41:21 +0300

    Simple and confirm Alerts

--- Sellenium/SelleniumIntro/src/Class12/_00_alertDemo.java
+++ Sellenium/SelleniumIntro/src/Class12/_00_alertDemo.java
@@ -5,15 +5,34 @@ import org.openqa.selenium.WebElement;
 
 import static utils.BaseClass.*;
 public class _00_alertDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         setUp();//https://selenium08.blogspot.com/2019/07/alert-test.html
-        driver.manage().window().maximize();
+
         WebElement SimpleAlert = driver.findElement(By.cssSelector("[id='simple']"));
         SimpleAlert.click();
         Alert simpleAlert = driver.switchTo().alert();
+        String simpleAlertText = simpleAlert.getText();
+        System.out.println("simpleAlertText = " + simpleAlertText);
         simpleAlert.accept();
+        
+        Thread.sleep(2000);
 
+        WebElement ConfirmAlert = driver.findElement(By.cssSelector("[id='confirm']"));
+        ConfirmAlert.click();
+        Alert confirmAlert = driver.switchTo().alert();
+        String confirmAlertText = confirmAlert.getText();
+        System.out.println("confirmAlertText = " + confirmAlertText);
+        confirmAlert.accept();
 
-        tearDown();
+        Thread.sleep(2000);
+
+        ConfirmAlert.click();
+        Alert confirmAlertNo = driver.switchTo().alert();
+        String confirmAlertNoText = confirmAlertNo.getText();
+        System.out.println("confirmAlertNoText = " + confirmAlertNoText);
+        confirmAlert.dismiss();
+        Thread.sleep(2000);
+
+        //tearDown();
     }
 }
