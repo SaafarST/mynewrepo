bd885f07a046892169f5f5ed7803b690804e3d5e
TohirJu <53301540+SaafarST@users.noreply.github.com>
2022-12-18 17:02:17 +0300

    Some commands in Selenium

---
+++ Sellenium/SelleniumIntro/src/Class12/_00_SomeCommands.java
@@ -0,0 +1,9 @@
+package Class12;
+import static utils.BaseClass.*;
+public class _00_SomeCommands {
+    public static void main(String[] args) {
+        setUp();
+        driver.manage().window().maximize();
+        tearDown();
+    }
+}
