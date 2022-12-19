4e95fc922b871ee3446dde47bed955c900cbff96
TohirJu <53301540+SaafarST@users.noreply.github.com>
2022-12-19 21:55:53 +0300

    Part 2: Modal Dialogue Popup

---
+++ Sellenium/SelleniumIntro/src/Assignments/_04_ModalDialoguePopup.java
@@ -0,0 +1,10 @@
+package Assignments;
+import static utils.BaseClass.*;
+
+public class _04_ModalDialoguePopup {
+    public static void main(String[] args) {
+        setUp();
+
+        tearDown();
+    }
+}
