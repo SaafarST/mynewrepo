5cf20c93adbb866b44ce291965e15e53f6263efe
TohirJu <53301540+SaafarST@users.noreply.github.com>
2022-12-19 22:22:04 +0300

    Part 2: Modal Dialogue Popup

--- Sellenium/SelleniumIntro/src/Assignments/_04_ModalDialoguePopup.java
+++ Sellenium/SelleniumIntro/src/Assignments/_04_ModalDialoguePopup.java
@@ -7,7 +7,7 @@ import static utils.BaseClass.*;
 
 public class _04_ModalDialoguePopup {
     //https://discord.com/channels/1002721597834149979/1002721598396170256/1053745403264241914
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         setUp();
 
         WebElement webElement = driver.findElement(By.cssSelector("[id='showLargeModal']"));
@@ -25,6 +25,7 @@ public class _04_ModalDialoguePopup {
 
        driver.findElement(By.cssSelector("[id='closeLargeModal']")).click();
 
+       Thread.sleep(1000);
 
         tearDown();
     }
