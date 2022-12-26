ded513c2bc233000f2b8da5fcf7d7480ac542852
TohirJu <53301540+SaafarST@users.noreply.github.com>
2022-12-26 23:53:13 +0500

    Authentication Alert type

--- Sellenium/SelleniumIntro/src/Class12/_02_AuthenticationAlert.java
+++ Sellenium/SelleniumIntro/src/Class12/_02_AuthenticationAlert.java
@@ -3,10 +3,13 @@ import static utils.BaseClass.*;
 
 
 public class _02_AuthenticationAlert {
-    public static void main(String[] args) {
-        setUp("https://httpbin.org/basiauth/foo/bar");
+    public static void main(String[] args) throws InterruptedException {
+        //setUp("https://httpbin.org/basic-auth/foo/bar");
+        setUp("https://foo:bar@httpbin.org/basic-auth/foo/bar");
 
 
+
+        Thread.sleep(1000);
 
         tearDown();
 
