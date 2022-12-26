cc03bbcd39c2b29b8e9658055b46b0ca3a558d89
TohirJu <53301540+SaafarST@users.noreply.github.com>
2022-12-26 23:39:07 +0500

    Authentication Alert type

---
+++ Sellenium/SelleniumIntro/src/Class12/_02_AuthenticationAlert.java
@@ -0,0 +1,14 @@
+package Class12;
+import static utils.BaseClass.*;
+
+
+public class _02_AuthenticationAlert {
+    public static void main(String[] args) {
+        setUp("https://httpbin.org/basiauth/foo/bar");
+
+
+
+        tearDown();
+
+    }
+}
