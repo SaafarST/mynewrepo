4fe6169d539a65fb61ca079a552431f8dbb298d6
TohirJu <53301540+SaafarST@users.noreply.github.com>
2022-12-26 23:54:46 +0500

    Authentication Alert type

---
+++ Sellenium/SelleniumIntro/src/Class12/_02_AuthenticationAlert_part2.java
@@ -0,0 +1,19 @@
+package Class12;
+
+import static utils.BaseClass.setUp;
+import static utils.BaseClass.tearDown;
+
+
+public class _02_AuthenticationAlert_part2 {
+    public static void main(String[] args) throws InterruptedException {
+        //setUp("https://the-internet.herokuapp.com/basic_auth");
+        setUp("https://admin:admin@the-internet.herokuapp.com/basic_auth");
+
+
+
+        Thread.sleep(1000);
+
+        tearDown();
+
+    }
+}
