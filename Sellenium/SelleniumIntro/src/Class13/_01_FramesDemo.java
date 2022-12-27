cb6559161787f96a0677331dcb8f241d4c19b774
TohirJu <53301540+SaafarST@users.noreply.github.com>
2022-12-27 17:00:10 +0500

    Frames

--- Sellenium/SelleniumIntro/src/Class13/_01_FramesDemo.java
+++ Sellenium/SelleniumIntro/src/Class13/_01_FramesDemo.java
@@ -1,11 +1,20 @@
 package Class13;
 
+import org.openqa.selenium.By;
+
 import static utils.BaseClass.*;
 
 public class _01_FramesDemo {
-    public static void main(String[] args) {
-        setUp("uitestpractice.com/Students/Switchto");
+    public static void main(String[] args) throws InterruptedException {
+        setUp("http://www.uitestpractice.com/");
+        driver.findElement(By.xpath("//*[contains(text(),'Switch to')]")).click();
+        String text = driver.findElement(By.xpath("//p[contains(text(),'Testing')]")).getText();
+        System.out.println("text = " + text);
 
+        driver.switchTo().frame(0);
+        driver.findElement(By.id("name")).sendKeys("Sdet");
+
+        Thread.sleep(1000);
 
         tearDown();
     }
