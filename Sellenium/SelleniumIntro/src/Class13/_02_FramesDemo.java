85d86ca6fa39c26063b5482d7c3dfed6bcea1cdb
TohirJu <53301540+SaafarST@users.noreply.github.com>
2022-12-27 23:23:43 +0500

    Frames: part 2

---
+++ Sellenium/SelleniumIntro/src/Class13/_02_FramesDemo.java
@@ -0,0 +1,16 @@
+package Class13;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static utils.BaseClass.*;
+
+public class _02_FramesDemo {
+    public static void main(String[] args) throws InterruptedException {
+        setUp("http://www.uitestpractice.com/");
+
+
+        tearDown();
+    }
+
+}
