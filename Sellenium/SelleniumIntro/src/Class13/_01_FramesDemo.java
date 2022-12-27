1cb97b7b628674998d5463bef1d6b10923623b64
TohirJu <53301540+SaafarST@users.noreply.github.com>
2022-12-27 17:10:37 +0500

    Frames

--- Sellenium/SelleniumIntro/src/Class13/_01_FramesDemo.java
+++ Sellenium/SelleniumIntro/src/Class13/_01_FramesDemo.java
@@ -12,6 +12,7 @@ public class _01_FramesDemo {
         String text = driver.findElement(By.xpath("//p[contains(text(),'Testing')]")).getText();
         System.out.println("text = " + text);
 
+        //1. By index:
         driver.switchTo().frame(0);
         WebElement textBox = driver.findElement(By.id("name"));
         textBox.sendKeys("Sdet");
@@ -19,6 +20,15 @@ public class _01_FramesDemo {
         Thread.sleep(1000);
         textBox.clear();
         textBox.sendKeys("QA Engineer");
+
+        driver.switchTo().defaultContent();
+        String text1 = driver.findElement(By.xpath("//p[contains(text(),'Testing')]")).getText();
+        System.out.println("text = " + text1);
+
+        //2. By ID or Name:
+        driver.switchTo().frame("iframe_a");
+        textBox.clear();
+        textBox.sendKeys("Hi");
 
 
 
