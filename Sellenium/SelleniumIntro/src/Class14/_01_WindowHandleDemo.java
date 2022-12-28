a9b80fb7f370ce40a98ebcf7fc92308d7e21d245
TohirJu <53301540+SaafarST@users.noreply.github.com>
2022-12-29 02:29:47 +0500

    Window Handle

--- Sellenium/SelleniumIntro/src/Class14/_01_WindowHandleDemo.java
+++ Sellenium/SelleniumIntro/src/Class14/_01_WindowHandleDemo.java
@@ -1,8 +1,40 @@
 package Class14;
+import org.openqa.selenium.By;
+
+import java.util.Set;
+
 import static utils.BaseClass.*;
 public class _01_WindowHandleDemo {
     public static void main(String[] args) {
         setUp("https://the-internet.herokuapp.com/windows");
+        /**
+         *  How to handle new TABS and new WINDOWS in Selenium?
+         *          *  We have two methods for handling new tabs/windows.
+         *          *      1. getWindowHandle();    ==> handles main window only
+         *          *      2. getWindowHandles();   ==> handles ALL windows (or tabs)
+         *
+         *          */
+
+        String mainWindowTitle = driver.getTitle();
+        System.out.println("Main Window Title = " + mainWindowTitle);
+
+        String windowHandle = driver.getWindowHandle();
+        System.out.println("windowHandle = " + windowHandle);
+
+        driver.findElement(By.linkText("Click Here")).click();
+        driver.findElement(By.linkText("Click Here")).click();
+        driver.findElement(By.linkText("Click Here")).click();
+        driver.findElement(By.linkText("Click Here")).click();
+        
+        String WindowTitle = driver.getTitle();
+        System.out.println("Window Title = " + WindowTitle);
+
+        Set<String> windowHandles = driver.getWindowHandles();
+        System.out.println("windowHandles = " + windowHandles);
+        int allOpenWindows = windowHandles.size();
+        System.out.println("All tabs currentky open = " + allOpenWindows);
+
+        //driver.switchTo().window(windowHandles.)
 
         tearDown();
 
