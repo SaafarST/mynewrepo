ba3c5711a8e861872463e4712f2d26763d1d7c83
TohirJu <53301540+SaafarST@users.noreply.github.com>
2022-12-28 17:58:23 +0500

    Nested Frames

---
+++ Sellenium/SelleniumIntro/src/Class13/_04_NestedFrames.java
@@ -0,0 +1,11 @@
+package Class13;
+import static utils.BaseClass.*;
+public class _04_NestedFrames {
+    public static void main(String[] args) {
+        setUp("http://ui.vision/demo/webtest/frames/");
+
+
+
+        tearDown();
+    }
+}
