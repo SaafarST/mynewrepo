fff1048241fe6221870f6ed4e587ee2e0e553758
TohirJu <53301540+SaafarST@users.noreply.github.com>
2022-12-29 01:57:07 +0500

    Upload Files

--- Sellenium/SelleniumIntro/src/Class12/_04_UploadFiles.java
+++ Sellenium/SelleniumIntro/src/Class12/_04_UploadFiles.java
@@ -21,13 +21,22 @@ public class _04_UploadFiles {
          *
          */
         //Step 1: Specify file location - get full path of the file you are about to upload:
-        String filePath = "/Users/tohirsaidzoda/Downloads/Class03";
+        String filePath = "/Users/tohirsaidzoda/Downloads/java_string_methods.pdf";
 
         //Step 2: Locate web element for 'Choose file' button, and using sendKeys() method, send the location/full path of the file:
-        driver.findElement(By.id("file=upload")).sendKeys(filePath);
+        driver.findElement(By.id("file-upload")).sendKeys(filePath);
 
         //Step 3: Locate 'Upload' button element and click it to upload your file:
         driver.findElement(By.id("file-submit")).click();
+
+        //Check whether upload was successful:
+        String uploadStatus = driver.findElement(By.xpath("//h3[text()='File Uploaded!']")).getText();
+        if (uploadStatus.contains("Uploaded"))
+        {
+            System.out.println("File Upload successful!");
+        }else {
+            System.out.println("Upload failed");
+        }
 
         Thread.sleep(1000);
 
