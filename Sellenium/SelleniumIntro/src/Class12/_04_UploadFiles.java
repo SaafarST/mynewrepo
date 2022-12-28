65b02a1811e7541f61c000ee7511b530e839a963
TohirJu <53301540+SaafarST@users.noreply.github.com>
2022-12-29 01:44:37 +0500

    Upload Files

--- Sellenium/SelleniumIntro/src/Class12/_04_UploadFiles.java
+++ Sellenium/SelleniumIntro/src/Class12/_04_UploadFiles.java
@@ -1,7 +1,8 @@
 package Class12;
 
-import static utils.BaseClass.setUp;
-import static utils.BaseClass.tearDown;
+import org.openqa.selenium.By;
+
+import static utils.BaseClass.*;
 
 
 public class _04_UploadFiles {
@@ -9,6 +10,24 @@ public class _04_UploadFiles {
 
         setUp("https://the-internet.herokuapp.com/upload");
 
+        /**
+         * ==== How to Upload files using Selenium ======
+         *  How do we upload files using Selenium?
+         *  1. Specify file location - get full path of the file you are about to upload
+         *  2. Locate web element for 'Choose file' button, and using sendKeys() method, send the location/full path of the file.
+         *  3. Locate 'Upload' button element and click it to upload your file
+         *
+         *      This way we bypass the window popup, which is not supported by Selenium. (Just like we did with Authentication Alert)
+         *
+         */
+        //Step 1: Specify file location - get full path of the file you are about to upload:
+        String filePath = "/Users/tohirsaidzoda/Downloads/Class03";
+
+        //Step 2: Locate web element for 'Choose file' button, and using sendKeys() method, send the location/full path of the file:
+        driver.findElement(By.id("file=upload")).sendKeys(filePath);
+
+        //Step 3: Locate 'Upload' button element and click it to upload your file:
+        driver.findElement(By.id("file-submit")).click();
 
         Thread.sleep(1000);
 
