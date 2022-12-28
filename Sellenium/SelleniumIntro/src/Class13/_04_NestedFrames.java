37b4098d16965e3c593460c8b3ed8a6fb45551c8
TohirJu <53301540+SaafarST@users.noreply.github.com>
2022-12-28 18:31:10 +0500

    Nested Frames

--- Sellenium/SelleniumIntro/src/Class13/_04_NestedFrames.java
+++ Sellenium/SelleniumIntro/src/Class13/_04_NestedFrames.java
@@ -8,9 +8,10 @@ public class _04_NestedFrames {
         setUp("http://ui.vision/demo/webtest/frames/");
         WebDriver frame_3 = driver.switchTo().frame(driver.findElement(By.cssSelector("frame[src='frame_3.html']")));
         frame_3.findElement(By.name("mytext3")).sendKeys("Frame Three");
-        WebDriver frameInsideFrame = driver.switchTo().frame(driver.findElement(By.xpath("/html/body/center/p")));
-        String text = frameInsideFrame.findElement(By.xpath("(//*[contains(text(),'Form ')])[1]")).getText();
-        System.out.println("text = " + text);
+        WebDriver frameInsideFrame = driver.switchTo().frame(driver.findElement(By.cssSelector("body > center > iframe")));
+        String formTitle = driver.findElement(By.xpath("(//*[contains(text(),'Form ')])[1]")).getText();
+
+        System.out.println("Form Title = " + formTitle);
 
         tearDown();
     }
