b67fbf5a718c2989762b77fd22849668c910c2b1
TohirJu <53301540+SaafarST@users.noreply.github.com>
2022-12-29 02:15:49 +0500

    Window Handle

---
+++ Sellenium/SelleniumIntro/src/Class14/_01_WindowHandleDemo.java
@@ -0,0 +1,10 @@
+package Class14;
+import static utils.BaseClass.*;
+public class _01_WindowHandleDemo {
+    public static void main(String[] args) {
+        setUp("https://the-internet.herokuapp.com/windows");
+
+        tearDown();
+
+    }
+}
