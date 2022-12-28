89ccaa2a1cb0b6b5d37cf880ed1e369cad6457e9
TohirJu <53301540+SaafarST@users.noreply.github.com>
2022-12-29 01:35:15 +0500

    Nested Frames

--- Sellenium/SelleniumIntro/src/Class12/_03_AuthenticationAlert_part2.java
+++ Sellenium/SelleniumIntro/src/Class12/_03_AuthenticationAlert_part2.java
@@ -4,7 +4,7 @@ import static utils.BaseClass.setUp;
 import static utils.BaseClass.tearDown;
 
 
-public class _02_AuthenticationAlert_part2 {
+public class _03_AuthenticationAlert_part2 {
     public static void main(String[] args) throws InterruptedException {
         //setUp("https://the-internet.herokuapp.com/basic_auth");
         setUp("https://admin:admin@the-internet.herokuapp.com/basic_auth");
---
+++ Sellenium/SelleniumIntro/src/Class12/_04_UploadFiles.java
@@ -0,0 +1,19 @@
+package Class12;
+
+import static utils.BaseClass.setUp;
+import static utils.BaseClass.tearDown;
+
+
+public class _04_UploadFiles {
+    public static void main(String[] args) throws InterruptedException {
+
+        setUp("https://the-internet.herokuapp.com/upload");
+
+
+
+        Thread.sleep(1000);
+
+        tearDown();
+
+    }
+}
