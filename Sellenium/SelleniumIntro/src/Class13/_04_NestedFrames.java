2f13b0ceae05961693453ac49a249650a7690d30
TohirJu <53301540+SaafarST@users.noreply.github.com>
2022-12-29 01:20:39 +0500

    Nested Frames

--- Sellenium/SelleniumIntro/src/Class13/_04_NestedFrames.java
+++ Sellenium/SelleniumIntro/src/Class13/_04_NestedFrames.java
@@ -9,9 +9,9 @@ public class _04_NestedFrames {
         WebDriver frame_3 = driver.switchTo().frame(driver.findElement(By.cssSelector("frame[src='frame_3.html']")));
         frame_3.findElement(By.name("mytext3")).sendKeys("Frame Three");
         WebDriver frameInsideFrame = driver.switchTo().frame(driver.findElement(By.cssSelector("body > center > iframe")));
-        String formTitle = driver.findElement(By.xpath("(//*[contains(text(),'Form ')])[1]")).getText();
+        String formTitleText = driver.findElement(By.xpath("(//*[contains(text(),'Form ')])[2]")).getText();
 
-        System.out.println("Form Title = " + formTitle);
+        System.out.println("Form Title = " + formTitleText);
 
         tearDown();
     }
