bcde0f4a39b5852ac28092d3bb2069f8ce10c12a
TohirJu <53301540+SaafarST@users.noreply.github.com>
2022-12-30 01:55:48 +0500

    Window Handle

--- Sellenium/SelleniumIntro/src/Class02/_01_NavigationCommands.java
+++ Sellenium/SelleniumIntro/src/Class02/_01_NavigationCommands.java
@@ -5,17 +5,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class _01_NavigationCommands {
     public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
         WebDriver webDriver = new ChromeDriver();
         webDriver.get("https://www.google.com");
         Thread.sleep(2000);
 
         webDriver.navigate().to("https://yahoo.com");
         Thread.sleep(2000);
+
         webDriver.navigate().back();
         Thread.sleep(2000);
+
         webDriver.navigate().forward();
         Thread.sleep(2000);
+
         webDriver.quit();
         webDriver.close();
 
