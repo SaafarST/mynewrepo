3b97a33290aa954f148afcec4eadf824fc483db1
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-05 02:09:42 +0500

    Windows Handle example

--- Sellenium/SelleniumIntro/src/Class14/_03_WindowHandle.java
+++ Sellenium/SelleniumIntro/src/Class14/_03_WindowHandle.java
@@ -14,36 +14,6 @@ public class _03_WindowHandle {
         String parentWindow = driver.getWindowHandle();
         System.out.println("Parent Window = " + parentWindow);
 
-        driver.switchTo().newWindow(WindowType.TAB);//opens new BLANK tab and switch focus to it
-        driver.get("https://about.google/");
-
-        driver.switchTo().newWindow(WindowType.TAB);//opens new BLANK tab and switch focus to it
-        driver.get("https://store.google.com");
-
-        driver.switchTo().newWindow(WindowType.TAB);//opens new BLANK tab and switch focus to it
-        driver.get("https://ok.ru");
-
-        Set<String> allWindows = driver.getWindowHandles();
-        System.out.println("Total tabs open: "+allWindows.size());
-
-        int count = 1;
-        Iterator<String> iterator = allWindows.iterator();
-        while (iterator.hasNext()){
-            String nextWindow = iterator.next();
-            driver.switchTo().window(nextWindow);
-            String title = driver.getTitle();
-
-            System.out.println("Page"+count+" title:"+title);
-            count++;
-
-            List<String> windowList = new ArrayList<>();
-            windowList.add(nextWindow);
-            if (title.contains("OK")){
-                driver.switchTo().window(nextWindow);
-                System.out.println("Windows is found and switched to "+title);
-                break;
-            }
-        }
 
         tearDown();
     }
