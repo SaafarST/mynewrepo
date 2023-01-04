70d83e8a290f38de11496f7e81e3b94d351387c1
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-05 02:33:25 +0500

    Windows Handle example

--- Sellenium/SelleniumIntro/src/Class14/_03_WindowHandle.java
+++ Sellenium/SelleniumIntro/src/Class14/_03_WindowHandle.java
@@ -3,6 +3,7 @@ package Class14;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.util.Iterator;
 import java.util.Set;
 
 import static utils.BaseClass.*;
@@ -17,17 +18,27 @@ public class _03_WindowHandle {
         System.out.println("Parent Window = " + parentWindow);
 
         for (int i = 0; i <3; i++) {
-            driver.findElement(By.id("tabButton")).click();
+            //driver.findElement(By.id("tabButton")).click();//Open in a new Tab
+            driver.findElement(By.id("windowButton")).click();//Open in an new Window
             Thread.sleep(1000);
         }
 
-        Set<String> allWindows = driver.getWindowHandles();
-        for (String Window : allWindows) {
-            driver.switchTo().window(Window);
+//        Set<String> allWindows = driver.getWindowHandles();
+//        for (String Window : allWindows) {
+//            driver.switchTo().window(Window);
+//            driver.get("https://google.com");
+//            System.out.println("Window ID: "+Window+", window title "+driver.getTitle());
+//        }
+        Iterator<String> iterators = allWindows.iterator();
+        while (iterators.hasNext()){
+            String next = iterators.next();
+            driver.switchTo().window(next);
             driver.get("https://google.com");
-            System.out.println("Window ID:"+Window+", window title "+driver.getTitle());
+            System.out.println("Window ID: "+next+", window title "+driver.getTitle());
+
         }
         Thread.sleep(2000);
+
 
         tearDown();
     }
