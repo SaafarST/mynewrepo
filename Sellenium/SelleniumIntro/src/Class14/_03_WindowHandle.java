ae50e7cb7711952599c0779c466f4316475d6f06
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-05 02:14:54 +0500

    Windows Handle example

--- Sellenium/SelleniumIntro/src/Class14/_03_WindowHandle.java
+++ Sellenium/SelleniumIntro/src/Class14/_03_WindowHandle.java
@@ -6,6 +6,8 @@ public class _03_WindowHandle {
     public static void main(String[] args) throws InterruptedException {
         setUp("https://demoqa.com/browser-windows");
 
+        String driverTitle = driver.getTitle();
+        System.out.println("Driver title = " + driverTitle);
         String parentWindow = driver.getWindowHandle();
         System.out.println("Parent Window = " + parentWindow);
 
