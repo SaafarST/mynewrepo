4eaf4337d72bcf43c50f5a28347d9373bd654a66
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-06 00:43:59 +0500

    Wait demo

--- Sellenium/SelleniumIntro/src/Class15/_01_WaitDemo.java
+++ Sellenium/SelleniumIntro/src/Class15/_01_WaitDemo.java
@@ -2,15 +2,19 @@ package Class15;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.time.Duration;
+
 import static utils.BaseClass.*;
 public class _01_WaitDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         setUp("https://the-internet.herokuapp.com/dynamic_loading/2");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));//This is implicit wait and dynamic
 
-        driver.findElement(By.cssSelector("div#start button")).click(); //
-        WebElement helloWorldText = driver.findElement(By.cssSelector("div#finish h4"));
-        System.out.println("helloWorldText.getText() = " + helloWorldText.getText());
+        driver.findElement(By.cssSelector("div#start button")).click(); //Locate 'Start' button element and click on it
 
-        //tearDown();
+        WebElement helloWorldText = driver.findElement(By.cssSelector("div#finish h4"));//Locate Hello World web element and store
+        System.out.println("helloWorldText.getText() = " + helloWorldText.getText());//Print the text
+
+        tearDown();
     }
 }
