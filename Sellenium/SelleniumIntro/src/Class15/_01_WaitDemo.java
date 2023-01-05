303e919f25eadbab9a71caa9e60428d79a13239b
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-06 00:35:47 +0500

    Wait demo

--- Sellenium/SelleniumIntro/src/Class15/_01_WaitDemo.java
+++ Sellenium/SelleniumIntro/src/Class15/_01_WaitDemo.java
@@ -1,9 +1,16 @@
 package Class15;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 import static utils.BaseClass.*;
 public class _01_WaitDemo {
     public static void main(String[] args) {
-        setUp();
+        setUp("https://the-internet.herokuapp.com/dynamic_loading/2");
 
-        tearDown();
+        driver.findElement(By.cssSelector("div#start button")).click(); //
+        WebElement helloWorldText = driver.findElement(By.cssSelector("div#finish h4"));
+        System.out.println("helloWorldText.getText() = " + helloWorldText.getText());
+
+        //tearDown();
     }
 }
