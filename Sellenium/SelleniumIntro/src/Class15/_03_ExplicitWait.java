bcaaaf33200238b26c5507914221a7461b78dbde
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-07 00:42:50 +0500

    Explicit wait

---
+++ Sellenium/SelleniumIntro/src/Class15/_03_ExplicitWait.java
@@ -0,0 +1,29 @@
+package Class15;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+
+import java.time.Duration;
+
+import static utils.BaseClass.*;
+
+public class _03_ExplicitWait {
+    public static void main(String[] args) {
+
+        setUp("http://www.uitestpractice.com");
+
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        try {
+            driver.findElement(By.xpath("//*[contains(text(),'AjaxCall')]")).click();
+            driver.findElement(By.xpath("//*[contains(text(),'This is a Ajax link')]")).click();
+            WebElement text = driver.findElement(By.className("ContactUs"));
+            System.out.println("text.getText() = " + text.getText());
+        }catch (NoSuchElementException e){
+            e.printStackTrace();
+            System.out.println("Element is not found. Wait longer for the element to be visible on UI.");
+        }
+
+        tearDown();
+    }
+}
