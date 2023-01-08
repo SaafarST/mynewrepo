78c29056b27d5423eb676559fe1c4299ab19f582
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-08 19:16:12 +0300

    Explicit wait-minor changes

---
+++ Sellenium/SelleniumIntro/src/Class15/_05_ExplicitWait.java
@@ -0,0 +1,10 @@
+package Class15;
+import static utils.BaseClass.*;
+
+public class _05_ExplicitWait {
+    public static void main(String[] args) {
+        setUp("https://omayo.blogspot.com");
+
+        tearDown();
+    }
+}
