905817187635ad45ff9923ec53282b988bfa394e
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-08 19:44:39 +0300

    Fluent wait

---
+++ Sellenium/SelleniumIntro/src/Class15/_06_FluentWait.java
@@ -0,0 +1,30 @@
+package Class15;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static utils.BaseClass.*;
+
+public class _06_FluentWait {
+    public static void main(String[] args) {
+        setUp("https://omayo.blogspot.com");
+
+        FluentWait<WebDriver> fluentWait = new FluentWait<>(driver); //declare and initialize FluentWait
+        fluentWait.withTimeout(Duration.ofSeconds(15)); //Wait for up to given time (15 seconds)
+        fluentWait.pollingEvery(Duration.ofSeconds(1)); //Keep checking for the presence of element every 1 second
+        fluentWait.ignoring(NoSuchElementException.class);//Do not throw given exception until max time (15 seconds)
+
+        driver.findElement(By.cssSelector("button[class='dropbtn']")).click();//Locating Dropdown and click
+
+        driver.findElement(By.partialLinkText("Face")).click();//click to it
+
+
+
+        tearDown();
+    }
+}
