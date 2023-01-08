f4d4415b6000a3638f2637d5dc11bff3c3905138
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-08 19:33:09 +0300

    Explicit new example

--- Sellenium/SelleniumIntro/src/Class15/_05_ExplicitWait.java
+++ Sellenium/SelleniumIntro/src/Class15/_05_ExplicitWait.java
@@ -1,9 +1,24 @@
 package Class15;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 import static utils.BaseClass.*;
 
 public class _05_ExplicitWait {
     public static void main(String[] args) {
         setUp("https://omayo.blogspot.com");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        driver.findElement(By.cssSelector("button[class='dropbtn']")).click();//Locating Dropdown and click
+        wait.until(ExpectedConditions.elementToBeClickable((By.linkText("Facebook"))));//We wait for facebook link to be clickable
+        driver.findElement(By.partialLinkText("Face")).click();//click to it
+
 
 
         tearDown();
