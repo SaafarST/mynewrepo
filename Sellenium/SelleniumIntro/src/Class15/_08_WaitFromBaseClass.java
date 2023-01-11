cd9b265b7e74f301c07789f69d8837663020a616
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-11 18:47:59 +0300

    Wait from BaseClass

---
+++ Sellenium/SelleniumIntro/src/Class15/_08_WaitFromBaseClass.java
@@ -0,0 +1,23 @@
+package Class15;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+
+import java.time.Duration;
+
+import static utils.BaseClass.*;
+
+public class _08_WaitFromBaseClass {
+    public static void main(String[] args) {
+        setUp("https://omayo.blogspot.com");
+
+
+        driver.findElement(By.cssSelector("button[class='dropbtn']")).click();//Locating Dropdown and click
+
+        driver.findElement(By.partialLinkText("Face")).click();//click to it
+
+        tearDown();
+    }
+}
