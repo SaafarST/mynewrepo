bea74f89b35c08c72d00ca85901382948760dfaa
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-11 09:06:14 +0300

    Some minor changes

---
+++ Sellenium/SelleniumIntro/src/Class15/_07_PageLoadTimeout.java
@@ -0,0 +1,11 @@
+package Class15;
+import static utils.BaseClass.*;
+
+public class _07_PageLoadTimeout {
+    public static void main(String[] args) {
+        //setUp("https://exelenter.com");
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
+
+        tearDown();
+    }
+}
