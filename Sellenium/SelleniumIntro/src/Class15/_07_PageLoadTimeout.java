d406417e7fe74cd2e8377a700e11861cbc744a98
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-11 09:07:14 +0300

    Page Load Timeout

--- Sellenium/SelleniumIntro/src/Class15/_07_PageLoadTimeout.java
+++ Sellenium/SelleniumIntro/src/Class15/_07_PageLoadTimeout.java
@@ -1,10 +1,14 @@
 package Class15;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import static utils.BaseClass.*;
 
 public class _07_PageLoadTimeout {
     public static void main(String[] args) {
         //setUp("https://exelenter.com");
         System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
 
         tearDown();
     }
