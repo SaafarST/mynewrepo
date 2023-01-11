01a5a4798a93ab7b68b8c8ac37496966d51996c9
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-11 18:44:15 +0300

    Page Load Timeout

--- Sellenium/SelleniumIntro/src/Class15/_07_PageLoadTimeout.java
+++ Sellenium/SelleniumIntro/src/Class15/_07_PageLoadTimeout.java
@@ -1,31 +1,36 @@
 package Class15;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 import java.time.Instant;
 
-import static utils.BaseClass.*;
-
 public class _07_PageLoadTimeout {
     public static void main(String[] args) {
         //setUp("https://exelenter.com");
-        System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
         WebDriver driver = new ChromeDriver();
 
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(2));
 
         Instant startTime = Instant.now();
         System.out.println("startTime = " + startTime);
-        driver.get("https://exelenter.com");
-        
+
+        try {
+            driver.get("https://google.com");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Too slow to load, add more time!");
+        }
         Instant endTime = Instant.now();
         System.out.println("endTime = " + endTime);
 
         Duration pageLoadTime = Duration.between(startTime, endTime);
-        System.out.println("pageLoadTime = " + pageLoadTime);
+        System.out.println("pageLoadTime = " + pageLoadTime.toMillis());
 
         System.out.println(driver.getTitle());
+
         driver.close();
 
     }
