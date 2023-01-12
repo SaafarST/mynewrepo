0e03ad0817296828e8bcc115e4f68472cf627bc9
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-12 08:59:27 +0300

    LoginTest continued

--- Sellenium/SelleniumIntro/src/Class16_DevelopersFunctions/_01_LoginTest.java
+++ Sellenium/SelleniumIntro/src/Class16_DevelopersFunctions/_01_LoginTest.java
@@ -1,11 +1,24 @@
 package Class16_DevelopersFunctions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 import static utils.BaseClass.*;
 
 public class _01_LoginTest {
     public static void main(String[] args) {
-        setUp("https://exelenter.com");
+        setUp("https://exelentersdet.com");
+
+
+        WebElement username = driver.findElement(By.id("txtUsername"));
+        //This is without function:
+        username.sendKeys("Admin");
+
+        //This is with function:
+        sendText(username,"Admin");
 
         tearDown();
-
+    }
+    public static void sendText(WebElement element, String value){
+        element.sendKeys(value);
     }
 }
