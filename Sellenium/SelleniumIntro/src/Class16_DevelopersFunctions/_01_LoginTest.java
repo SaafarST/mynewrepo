938b609c1b4243fd559a5179dfb8077139edbc60
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-12 08:53:59 +0300

    LoginTest continued

--- Sellenium/SelleniumIntro/src/Class16_DevelopersFunctions/_01_LoginTest.java
+++ Sellenium/SelleniumIntro/src/Class16_DevelopersFunctions/_01_LoginTest.java
@@ -1,4 +1,11 @@
 package Class16_DevelopersFunctions;
+import static utils.BaseClass.*;
 
 public class _01_LoginTest {
+    public static void main(String[] args) {
+        setUp("https://exelenter.com");
+
+        tearDown();
+
+    }
 }
