b88a540d23e54a8f5534bf4862a7d351c3d10ec4
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-13 09:53:50 +0300

    dropDown mehod added

---
+++ Sellenium/SelleniumIntro/src/Class16_DevelopersFunctions/_03_DropDown.java
@@ -0,0 +1,25 @@
+package Class16_DevelopersFunctions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+import static utils.BaseClass.*;
+public class _03_DropDown {
+    public static void main(String[] args) {
+        setUp("https://selenium08.blogspot.com/2019/11/dropdown.html");
+
+        String expectedText = "June";
+        List<WebElement> listOfMonths = driver.findElements(By.cssSelector("select[name='Month'] option"));
+        for (WebElement month : listOfMonths) {
+            String actualText = month.getText();
+            if (actualText.equalsIgnoreCase(expectedText))
+            {
+                month.click();
+                break;
+            }
+        }
+
+        tearDown();
+    }
+}
