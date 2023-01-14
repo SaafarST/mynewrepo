d56960860300a361dffd2a16d76fc5ff1d906d47
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-14 23:45:07 +0300

    DropDown continued

---
+++ Sellenium/SelleniumIntro/src/Class16_DevelopersFunctions/_04_DropDown2.java
@@ -0,0 +1,32 @@
+package Class16_DevelopersFunctions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
+import static utils.BaseClass.*;
+
+public class _04_DropDown2 {
+    public static void main(String[] args) {
+        setUp("https://selenium08.blogspot.com/2019/11/dropdown.html");
+
+        String expectedText = "June";
+        List<WebElement> listOfMonths = driver.findElements(By.cssSelector("select[name='Month'] option"));
+        selectDdValue(listOfMonths,expectedText);
+
+        WebElement monthsDD = driver.findElement(By.cssSelector("select[name='Month']"));
+        Select select = new Select(monthsDD);
+
+        List<WebElement> months = select.getOptions();
+        for (WebElement month : months) {
+            if (month.getText().equals(expectedText)){
+                select.selectByVisibleText(expectedText);
+                break;
+            }
+        }
+
+        tearDown();
+    }
+
+}
