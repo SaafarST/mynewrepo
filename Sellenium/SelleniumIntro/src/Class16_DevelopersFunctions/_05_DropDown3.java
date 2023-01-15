514457d02b1f0d27f65a4f736a68c8cf901ad71e
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-15 15:26:32 +0300

    New dropdown example

---
+++ Sellenium/SelleniumIntro/src/Class16_DevelopersFunctions/_05_DropDown3.java
@@ -0,0 +1,19 @@
+package Class16_DevelopersFunctions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+import static utils.BaseClass.*;
+
+public class _05_DropDown3 {
+    public static void main(String[] args) {
+        setUp("https://selenium08.blogspot.com/2019/11/dropdown.html");
+
+        tearDown();
+    }
+
+
+
+
+}
