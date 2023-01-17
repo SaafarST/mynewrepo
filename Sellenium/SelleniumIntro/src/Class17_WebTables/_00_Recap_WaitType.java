73c90ef1289df80508639798d4f08bf50bc34381
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-17 23:00:25 +0300

    Waits recap

---
+++ Sellenium/SelleniumIntro/src/Class17_WebTables/_00_Recap_WaitType.java
@@ -0,0 +1,40 @@
+package Class17_WebTables;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static utils.BaseClass.*;
+public class _00_Recap_WaitType {
+    public static void main(String[] args) {
+        setUp("https://the-internet.herokuapp.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+
+        driver.findElement(By.xpath("//*[contains(text(),'Dynamic Controls')]")).click();
+        waitInSeconds(3);
+
+        WebElement checkboxBtn = driver.findElement(By.cssSelector("input[type='checkbox']"));
+        System.out.println(checkboxBtn.isSelected());
+        checkboxBtn.click();
+        System.out.println(checkboxBtn.isSelected());
+
+        WebElement button = driver.findElement(By.cssSelector("button[onclick='swapCheckbox()']"));
+        System.out.println(button.getText());
+        button.click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("message")));
+        String text = driver.findElement(By.id("message")).getText();
+        System.out.println(text);
+        System.out.println(button.getText());
+
+
+        waitInSeconds(1);
+        tearDown();
+    }
+}
