fbe0856bdf170f8ede95c42e1c4850700f8eaf10
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-18 23:10:38 +0300

    Tables are introduced, rows and columns methods introduced

--- Sellenium/SelleniumIntro/src/Class17_WebTables/_01_TablesDemo.java
+++ Sellenium/SelleniumIntro/src/Class17_WebTables/_01_TablesDemo.java
@@ -2,6 +2,7 @@ package Class17_WebTables;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.util.Iterator;
 import java.util.List;
 
 import static utils.BaseClass.*;
@@ -13,6 +14,28 @@ public class _01_TablesDemo {
         List<WebElement> rows = driver.findElements(By.cssSelector("table#table1 tbody tr"));
         int rowSize = rows.size();
         System.out.println("Row size is = " + rowSize);
+
+        System.out.println("Printing rows ===>");
+
+       /* Iterator<WebElement> iterator = rows.iterator();
+        while (iterator.hasNext()){
+            WebElement row = iterator.next();
+            String rowText = row.getText();
+            System.out.println("Row Text = " + rowText);
+        }*/
+
+        System.out.println("===Printing rows with less line===");
+        Iterator<WebElement> iterator1 = rows.iterator();
+        while (iterator1.hasNext()){
+            System.out.println(iterator1.next().getText());
+        }
+
+        System.out.println("===Printing columns===");
+        List<WebElement> columns = driver.findElements(By.xpath("//table[@id='table1']//th"));
+        System.out.println("number of columns" + columns.size());
+        for (WebElement column : columns) {
+            System.out.println(column.getText());
+        }
 
         tearDown();
     }
