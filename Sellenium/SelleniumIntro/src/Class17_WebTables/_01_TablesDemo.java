cee33b0cd40b9732244a3cded060a53ec083c1d0
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-18 22:50:11 +0300

    Tables are introduced

---
+++ Sellenium/SelleniumIntro/src/Class17_WebTables/_01_TablesDemo.java
@@ -0,0 +1,9 @@
+package Class17_WebTables;
+import static utils.BaseClass.*;
+
+public class _01_TablesDemo {
+    public static void main(String[] args) {
+        setUp("https://the-internet.herokuapp.com/tables");
+
+    }
+}
