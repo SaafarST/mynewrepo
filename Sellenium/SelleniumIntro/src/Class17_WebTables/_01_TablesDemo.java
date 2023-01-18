d06652d3eac19c578dd4a7a45e74fd44ba56a7ca
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-18 22:52:48 +0300

    Tables are introduced, continued

--- Sellenium/SelleniumIntro/src/Class17_WebTables/_01_TablesDemo.java
+++ Sellenium/SelleniumIntro/src/Class17_WebTables/_01_TablesDemo.java
@@ -1,9 +1,17 @@
 package Class17_WebTables;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
 import static utils.BaseClass.*;
 
 public class _01_TablesDemo {
     public static void main(String[] args) {
         setUp("https://the-internet.herokuapp.com/tables");
 
+        List<WebElement> rows = driver.findElements(By.cssSelector("table#table1 tbody tr"));
+
+        tearDown();
     }
 }
