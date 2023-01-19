be51cea7c92da94401f49ad347a9340fa21c3ef7
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-19 09:37:38 +0300

    Dynamic Tables are introduced

---
+++ Sellenium/SelleniumIntro/src/Class17_WebTables/_02_DynamicTable.java
@@ -0,0 +1,18 @@
+package Class17_WebTables;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.Iterator;
+import java.util.List;
+
+import static utils.BaseClass.*;
+
+public class _02_DynamicTable {
+    public static void main(String[] args) {
+        setUp("https://the-internet.herokuapp.com/tables");
+
+
+
+        tearDown();
+    }
+}
