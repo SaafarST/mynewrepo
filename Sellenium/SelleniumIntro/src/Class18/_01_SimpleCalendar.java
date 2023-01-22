e54c80a6d2086e77925005ce253b8688cac8288f
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-22 20:48:55 +0300

    Simple Calendars are introduced

--- Sellenium/SelleniumIntro/src/Class18/_01_SimpleCalendar.java
+++ Sellenium/SelleniumIntro/src/Class18/_01_SimpleCalendar.java
@@ -12,16 +12,18 @@ public class _01_SimpleCalendar {
         //Let's pick 17.10.2020:
         //Select date picker:
         driver.findElement(By.cssSelector("#datePickerMonthYearInput")).click();
+
         WebElement monthsDD = driver.findElement(By.cssSelector("select[class='react-datepicker__month-select']"));
         selectDdValue(monthsDD,9);
+
         WebElement yearsDD = driver.findElement(By.cssSelector("select[class='react-datepicker__year-select']"));
         selectDdValue(yearsDD,2020);
+
         List<WebElement> daysDD = driver.findElements(By.cssSelector("div.react-datepicker__week div"));
         selectDdValue(daysDD,"17");
 
         String pickerFieldText = driver.findElement(By.id("dateAndTimePickerInput")).getAttribute("value");
         System.out.println(pickerFieldText);
-
 
         tearDown();
     }
