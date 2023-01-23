a5e257eef0c3938cedc69a4997af7e277d23b1ec
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-23 08:50:57 +0300

    Calendars: assignment

---
+++ Sellenium/SelleniumIntro/src/Class18/_02_Assignment.java
@@ -0,0 +1,20 @@
+package Class18;
+
+public class _02_Assignment {
+/**
+ * HW: Test Case: Add and then Delete added employee from the table
+ * 1. Open Chrome Browser
+ * 2. Navigate to URL www.exelentersdet.com
+ * 3. Login to the website (Credentials are in your Properties file)
+ * 4. Add an employee
+ * 5. Verify employee has been added (By retrieving Employee ID).
+ * 6. Go to Employee list
+ * 7. Delete added employee (By employee ID - ID is unique, could be multiple employees with same name)
+ * 8. Quit the browser
+ */
+
+public static void main(String[] args) {
+
+}
+
+}
