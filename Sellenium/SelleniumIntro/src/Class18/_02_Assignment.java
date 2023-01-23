1e8891a1ae2a924f67c31c10df3f0b9514295d06
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-23 10:45:10 +0300

    Calendars: assignment continued

--- Sellenium/SelleniumIntro/src/Class18/_02_Assignment.java
+++ Sellenium/SelleniumIntro/src/Class18/_02_Assignment.java
@@ -2,6 +2,8 @@ package Class18;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 import static utils.BaseClass.*;
 public class _02_Assignment {
 /**
@@ -36,16 +38,41 @@ public static void main(String[] args) {
     driver.findElement(By.id("lastName")).sendKeys("Bell"); //set  surname to Bell
 
     String employeeID = driver.findElement(By.id("employeeId")).getAttribute("value");//Store employeeID
+    System.out.println("Employee ID " + employeeID);
 
     driver.findElement(By.id("btnSave")).click();//Click Save btn to add employee
 
     //driver.findElement(By.id("menu_pim_viewPimModule")).click();
     driver.findElement(By.id("menu_pim_viewEmployeeList")).click();//navigate to employee list again
 
-    driver.findElement(By.xpath("(//*[contains(text(),'Next')])[1]"));
+    WebElement nextBtn = driver.findElement(By.xpath("(//*[contains(text(),'Next')])[1]"));
 
+    List<WebElement> selectElements = driver.findElements(By.cssSelector("tbody tr td:nth-child(1)"));//Get all select elements
+    List<WebElement> listOfIDs = driver.findElements(By.cssSelector("tbody tr td:nth-child(2)"));//Get all list of ID's
 
-    //tearDown();
+    //Check element if it exists in the given list of elements:
+    for (int i = 0; i < listOfIDs.size(); i++) {
+        if (listOfIDs.get(i).getText().equals(employeeID)){
+            selectElements.get(i).click();
+            break;
+        }
+    }
+
+    WebElement deleteBtn = driver.findElement(By.id("btnDelete"));
+    deleteBtn.click();
+
+    WebElement dialogDeleteBtn = driver.findElement(By.id("dialogDeleteBtn"));//Dialogue delete Btn
+    dialogDeleteBtn.click();//click ok!
+
+    //Re-check element if it exists in the given list of elements:
+    for (int i = 0; i < listOfIDs.size(); i++) {
+        if (listOfIDs.get(i).getText().equals(employeeID)){
+            System.out.println("Element has not been deleted!");
+            break;
+        }
+    }
+
+    tearDown();
 
 }
 
