0aeaf5c13fd9ecbf30d6a02118eb1fe7c0cd3a5c
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-28 15:19:25 +0300

    Screenshots introduced

--- Sellenium/SelleniumIntro/src/Class19_Screenshots_Actions_JS/_01_ScreenshotsDemo.java
+++ Sellenium/SelleniumIntro/src/Class19_Screenshots_Actions_JS/_01_ScreenshotsDemo.java
@@ -1,9 +1,41 @@
 package Class19_Screenshots_Actions_JS;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import utils.ConfigsReader;
+
+import java.io.File;
+import java.io.IOException;
+
 import static utils.BaseClass.*;
 public class _01_ScreenshotsDemo {
     public static void main(String[] args) {
         setUp("https://exelentersdet.com");
 
+        // Login to the web application
+        sendText(driver.findElement(By.id("txtUsername")), ConfigsReader.getProperties("username"));
+        sendText(driver.findElement(By.id("txtPassword")), ConfigsReader.getProperties("password"));
+        clickButWaitForClickability(driver.findElement(By.id("btnLogin")));
+
+        /**
+         *  In Selenium 3, we use 'TakesScreenshot' interface to take snapshots of web pages.
+         *  For copying file from source (webpage) to destination (your local folder), we can use either:
+         *      1. FileUtils.copyFile(sourceFile, destinationFile)  <== from Commons IO, an Apache Library
+         *      2. FileHandler.copy(sourceFile, destinationFile)    <== from Selenium Library
+         *
+         *      Method used is:
+         *         - getScreenshotAs()
+         */
+
+        TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+        File sourceFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(sourceFile,new File("dashboard.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Screenshot is not taken");
+        }
 
 
         tearDown();
