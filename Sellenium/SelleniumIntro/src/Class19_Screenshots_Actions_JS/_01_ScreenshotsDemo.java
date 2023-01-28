10697b908411e04a72855c9fbd568e4541bed929
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-28 14:57:53 +0300

    Screenshots introduced

---
+++ Sellenium/SelleniumIntro/src/Class19_Screenshots_Actions_JS/_01_ScreenshotsDemo.java
@@ -0,0 +1,11 @@
+package Class19_Screenshots_Actions_JS;
+import static utils.BaseClass.*;
+public class _01_ScreenshotsDemo {
+    public static void main(String[] args) {
+        setUp("https://exelentersdet.com");
+
+
+
+        tearDown();
+    }
+}
