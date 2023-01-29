9feab09965c64540504e5bd2294543a3fa97bd1a
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-29 17:05:36 +0300

    DragAndDrop finished

--- Sellenium/SelleniumIntro/src/Class19_Screenshots_Actions_JS/_07_ActionsDragAndDrop.java
+++ Sellenium/SelleniumIntro/src/Class19_Screenshots_Actions_JS/_07_ActionsDragAndDrop.java
@@ -2,31 +2,32 @@ package Class19_Screenshots_Actions_JS;
 
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
 
 import static utils.BaseClass.*;
 
 public class _07_ActionsDragAndDrop {
     public static void main(String[] args) {
-        setUp("https://google.com/");
+        setUp("https://demoqa.com/droppable");
 
-        waitInSeconds(1);
-        driver.findElement(By.name("q")).sendKeys("Exelenter Sdet");
-        waitInSeconds(1);
-        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
-        waitInSeconds(1);
+        WebElement draggable = driver.findElement(By.id("draggable"));
+        WebElement droppable = driver.findElement(By.id("droppable"));
 
-        // another way to do above action
-        driver.navigate().back();
-        waitInSeconds(1);
-        driver.findElement(By.name("q")).sendKeys("Exelenter sdet"+Keys.ENTER);
+        Actions actions = new Actions(driver);
+
+        //1st way:
+        actions.clickAndHold(draggable).moveToElement(droppable).release().perform();
         waitInSeconds(1);
 
-        // another way to do above action
-        driver.navigate().back();
+        driver.navigate().refresh();
         waitInSeconds(1);
-        driver.findElement(By.name("q")).sendKeys(Keys.chord("Exelenter sdet",Keys.ENTER));
 
+        //2nd way:
+        draggable = driver.findElement(By.id("draggable"));
+        droppable = driver.findElement(By.id("droppable"));
+        actions.dragAndDrop(draggable,droppable).perform();
 
         waitInSeconds(1);
         tearDown();
