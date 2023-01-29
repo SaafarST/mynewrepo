9c92daf2c15c677552ea1f7e7d63833afff6b89a
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-29 14:57:18 +0300

    Actions introduced

--- Sellenium/SelleniumIntro/src/Class19_Screenshots_Actions_JS/_04_ActionsClass.java
+++ Sellenium/SelleniumIntro/src/Class19_Screenshots_Actions_JS/_04_ActionsClass.java
@@ -1,9 +1,13 @@
 package Class19_Screenshots_Actions_JS;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
 import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
 
 import static utils.BaseClass.*;
 public class _04_ActionsClass {
@@ -28,6 +32,28 @@ public class _04_ActionsClass {
         waitInSeconds(1);
         actions.contextClick(electronicsMenu).perform();
         waitInSeconds(1);
+        //electronicsMenu.sendKeys(Keys.ESCAPE);
+        //actions.sendKeys(Keys.ESCAPE).build().perform();
+
+        Robot robot = null;
+        try {
+            robot = new Robot();
+        } catch (AWTException e) {
+            throw new RuntimeException(e);
+        }
+        robot.keyPress(KeyEvent.VK_ESCAPE);
+        waitInSeconds(1);
+
+        driver.navigate().to("https://demoqa.com/buttons");
+        waitInSeconds(1);
+
+        actions.doubleClick(driver.findElement(By.id("doubleClickBtn"))).perform();
+        System.out.println(driver.findElement(By.id("doubleClickMessage")).getText());
+
+        WebElement rightClickBtn = driver.findElement(By.id("rightClickBtn"));
+        actions.contextClick(rightClickBtn).perform();
+
+        System.out.println(driver.findElement(By.id("rightClickMessage")).getText());
 
         waitInSeconds(1);
         tearDown();
