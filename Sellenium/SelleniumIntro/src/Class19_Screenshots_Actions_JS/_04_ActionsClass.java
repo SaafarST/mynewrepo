2c29b4c1e34092b26ccc3d56a603d1fb43b3bad1
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-29 13:52:12 +0300

    Actions introduced

--- Sellenium/SelleniumIntro/src/Class19_Screenshots_Actions_JS/_04_ActionsClass.java
+++ Sellenium/SelleniumIntro/src/Class19_Screenshots_Actions_JS/_04_ActionsClass.java
@@ -1,4 +1,6 @@
 package Class19_Screenshots_Actions_JS;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
 import javax.swing.*;
@@ -7,9 +9,27 @@ import static utils.BaseClass.*;
 public class _04_ActionsClass {
     public static void main(String[] args) {
         setUp("https://www.ebay.com");
+
+        WebElement myeBay = driver.findElement(By.linkText("Мой eBay"));
         Actions actions = new Actions(driver);
+        actions.moveToElement(myeBay).perform();
 
+        String summaryText = driver.findElement(By.xpath("(//ul[@id='gh-ul-nav']//a)[1]")).getText();
+        if (summaryText.contains("Краткий обзор")){
+            System.out.println("Hover over eBay is successfull.");
+        }else {
+            System.out.println("Hover did not work.");
+        }
 
-        //tearDown();
+        waitInSeconds(1);
+
+        WebElement electronicsMenu = driver.findElement(By.linkText("Электроника"));
+        actions.moveToElement(electronicsMenu).perform();
+        waitInSeconds(1);
+        actions.contextClick(electronicsMenu).perform();
+        waitInSeconds(1);
+
+        waitInSeconds(1);
+        tearDown();
     }
 }
