2b2e85bf5072a218016a2c48d27d7417b5aa6cbc
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-01-29 13:30:34 +0300

    Actions introduced

---
+++ Sellenium/SelleniumIntro/src/Class19_Screenshots_Actions_JS/_04_ActionsClass.java
@@ -0,0 +1,15 @@
+package Class19_Screenshots_Actions_JS;
+import org.openqa.selenium.interactions.Actions;
+
+import javax.swing.*;
+
+import static utils.BaseClass.*;
+public class _04_ActionsClass {
+    public static void main(String[] args) {
+        setUp("https://www.ebay.com");
+        Actions actions = new Actions(driver);
+
+
+        //tearDown();
+    }
+}
