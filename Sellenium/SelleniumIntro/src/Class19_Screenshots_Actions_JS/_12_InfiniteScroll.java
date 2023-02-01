57ca9618399807e83a12913a779fe8b31ce7d137
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-02-01 18:48:03 +0300

    More on JSExecutor.

---
+++ Sellenium/SelleniumIntro/src/Class19_Screenshots_Actions_JS/_12_InfiniteScroll.java
@@ -0,0 +1,35 @@
+package Class19_Screenshots_Actions_JS;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import utils.ConfigsReader;
+
+import java.util.List;
+
+import static utils.BaseClass.*;
+
+public class _12_InfiniteScroll {
+    public static void main(String[] args) {
+        setUp("https://the-internet.herokuapp.com/");
+        driver.findElement(By.linkText("Infinite Scroll")).click();
+        waitInSeconds(2);
+
+        List<WebElement> paragraphs = driver.findElements(By.cssSelector("div[class='jscroll-added']"));
+
+        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver;
+        String script = "window.scrollTo(0, document.body.scrollHeight)";
+        System.out.println("Number of paragraphs: "+paragraphs.size());
+
+        while (paragraphs.size()<10){
+            javascriptExecutor.executeScript(script);
+            paragraphs = driver.findElements(By.cssSelector("div[class='jscroll-added']"));
+        }
+
+
+        System.out.println("Number of paragraphs: "+paragraphs.size());
+
+        //tearDown();
+    }
+}
