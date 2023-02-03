b6b488bdbe6096f9379d490585bc2f567c46d6f0
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-02-03 12:07:36 +0300

    LoginPageWithoutPageFactory and loginTest introduced.

--- Sellenium/SelleniumIntro/src/Class20_POM/test/_01_LoginTest.java
+++ Sellenium/SelleniumIntro/src/Class20_POM/test/_01_LoginTest.java
@@ -1,11 +1,16 @@
 package Class20_POM.test;
+import Class20_POM.pages._01_LoginPageWithoutPageFactory;
+import utils.ConfigsReader;
+
 import static utils.BaseClass.*;
 public class _01_LoginTest {
     public static void main(String[] args) {
         setUp("https://exelentersdet.com/");
 
-
-
+        var loginPage = new _01_LoginPageWithoutPageFactory();
+        sendText(loginPage.username, ConfigsReader.getProperties("username"));
+        sendText(loginPage.password,ConfigsReader.getProperties("password"));
+        click(loginPage.loginBtn);
 
         tearDown();
     }
